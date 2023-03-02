a90c5084bf5dba41a03bddafcbce22cebc06495c
DESKTOP-CB82ULD\faidems8 <vnfmsthfdlv@gmail.com>
2023-03-02 15:01:20 +0900

    top버튼 스크립트 이동

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
 // 공통
+// top btn
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.btn-top').fadeIn(300);
+        } else {
+            $('.btn-top').fadeOut(300);
+        }
+    });
+    $('.btn-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    });
+})
